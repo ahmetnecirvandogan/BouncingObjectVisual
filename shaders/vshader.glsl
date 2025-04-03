55492aef54f6a600a305cc7690d9d01580439883
adayildizz <adayildiz22@ku.edu.tr>
2025-04-03 17:31:28 +0300

    vertex shader updated

--- shaders/vshader.glsl
+++ shaders/vshader.glsl
@@ -1,14 +1,18 @@
 #version 330 core
 
 in vec4 vPosition;
+in vec3 vNormal; // Added for lighting
+
+out vec3 Normal; // Pass to fragment shader
+out vec3 FragPos; // For lighting calculations
 
 uniform mat4 ModelView;
 uniform mat4 Projection;
-//uniform vec4 disp;
 
 void main()
 {
     gl_Position = Projection * ModelView * vPosition;
-    //gl_Position = Projection * (vPosition + disp);
+    FragPos = vec3(ModelView * vPosition);
+    Normal = mat3(transpose(inverse(ModelView))) * vNormal; // Transform normals correctly
     
 }
